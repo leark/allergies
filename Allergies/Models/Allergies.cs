d27e63b903b88ef34a00994847671cfff69d25c8
Seung Lee <shl7@uw.edu>
2022-07-12 13:15:49 -0700

    Add initial files

---
+++ Allergies/Models/Allergies.cs
@@ -0,0 +1,21 @@
+namespace Allergy
+{
+  public class Allergies
+  {
+    
+  }
+}
+
+/*
+
+allergen     score
+eggs         1
+peanuts      2
+shellfish    4
+strawberries 8
+tomatoes     16
+chocolate    32
+pollen       64
+cats         128
+
+*/
